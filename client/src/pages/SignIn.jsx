5e1d5a5a05296f9dc633675c3047b2b174efd37c
Enes Özdemir <ozdemirenes558@gmail.com>
2024-10-24 15:59:15 +0300

    create responsive design for sign up and sign in pages

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,9 +2,9 @@ import { Button, Label, TextInput } from "flowbite-react";
 
 export default function SignIn() {
   return (
-    <div className="bg-gray-100 rounded-lg mt-20 flex container mx-auto gap-x-16">
+    <div className="rounded-lg mt-10 sm:mt-20 flex flex-col sm:flex-row container mx-auto gap-x-16">
     {/* left side */}
-    <div className="flex-1 flex flex-col gap-y-4 p-8">
+    <div className="flex-1 flex flex-col gap-y-4 p-7">
         <div>
             <Label>Email</Label>
             <TextInput placeholder="Email" id="email" type="email" required />
@@ -16,18 +16,18 @@ export default function SignIn() {
         </div>
 
 
-        <div className="mt-4">
+        <div className="mt-7">
             <Button type="submit" className="w-full" gradientDuoTone="greenToBlue">
                 Sign In
             </Button>
-            <p className="text-gray-600 mt-2">New here? Create your account now! <a href="/sign-up" className="text-blue-500 ml-1 hover:underline">Sign Up</a></p>
+            <p className="text-gray-600 mt-2 text-sm">New here? Create your account now! <a href="/sign-up" className="text-blue-500 ml-1 hover:underline">Sign Up</a></p>
         </div>
     </div>
 
     {/* right side */}
-    <div className="flex-1 flex flex-col justify-center">
-        <h1 className="text-[6rem] font-airone font-light">Blog.</h1>
-        <p className="text-gray-400">Join a journey of discovery and inspiration, where each post unravels new ideas and sparks meaningful conversations.</p>
+    <div className="flex-1 flex flex-col justify-center px-7 sm:px-0">
+        <h1 className="text-[4rem] sm:text-[6rem] font-airone font-light">Blog.</h1>
+        <p className="text-gray-400 text-sm">Your journey continues! Dive back into the world of inspiration and thoughtful conversations.</p>
     </div>
 </div>
   )
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,9 +2,9 @@ import { Button, Label, TextInput } from "flowbite-react";
 
 export default function SignUp() {
   return (
-    <div className="bg-gray-100 rounded-lg mt-20 flex container mx-auto gap-x-16">
+    <div className=" rounded-lg mt-5 sm:mt-20 flex flex-col sm:flex-row container mx-auto gap-x-16">
         {/* left side */}
-        <div className="flex-1 flex flex-col gap-y-5 p-8">
+        <div className="flex-1 flex flex-col gap-y-5 p-7">
             <div>
                 <Label>Username</Label>
                 <TextInput placeholder="Username" id="username" type="text" required />
@@ -26,19 +26,19 @@ export default function SignUp() {
             </div>
 
 
-            <div className="mt-4">
+            <div className="mt-7">
                 <Button type="submit" className="w-full" gradientDuoTone="greenToBlue">
                     Sign Up
                 </Button>
-                <p className="text-gray-600 mt-2">Already have an account? <a href="/sign-in" className="text-blue-500 ml-1 hover:underline">Sign In</a></p>
+                <p className="text-gray-600 mt-2 text-sm">Already have an account? <a href="/sign-in" className="text-blue-500 ml-1 hover:underline">Sign In</a></p>
             </div>
         </div>
 
         {/* right side */}
-        <div className="flex-1 flex flex-col justify-center">
-            <h1 className="text-[6rem] font-airone font-light">Blog.</h1>
-            <p className="text-gray-400">Join a journey of discovery and inspiration, where each post unravels new ideas and sparks meaningful conversations.</p>
-        </div>
+        <div className="flex-1 flex flex-col justify-center px-7 sm:px-0">
+            <h1 className="text-[4rem] sm:text-[6rem] font-airone font-light">Blog.</h1>
+        <p className="text-gray-400 text-sm">Join a journey of discovery and inspiration, where each post unravels new ideas and sparks meaningful conversations.</p>
+    </div>
     </div>
   )
 }
