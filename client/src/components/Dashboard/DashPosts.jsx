4f00b3b99987a73ea4e331eee4c67ae4cac1f2f0
Enes Özdemir <ozdemirenes558@gmail.com>
2024-10-30 16:07:10 +0300

    create get posts API route

--- client/src/components/Dashboard/DashPosts.jsx
+++ client/src/components/Dashboard/DashPosts.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import { PiEqualsThin, PiXThin } from "react-icons/pi";
+import DashSidebar from "./DashSidebar";
+
 export default function DashPosts() {
+  const [dropdown, setDropdown] = useState(false);
   return (
-    <div>DashPosts</div>
-  )
+    <div className="flex flex-col sm:flex-row w-full">
+      <div className="flex justify-between w-full max-w-5xl px-5 py-5 sm:px-16">
+        <div className="font-normal flex-1">
+          <h1 className="font-medium text-2xl">Posts</h1>
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+        <div className="block sm:hidden">
+          {dropdown ? (
+            <PiXThin size={25} onClick={() => setDropdown(!dropdown)} />
+          ) : (
+            <PiEqualsThin size={25} onClick={() => setDropdown(!dropdown)} />
+          )}
+          {dropdown && (
+            <div className="absolute top-28 left-0 w-full h-full">
+              <DashSidebar />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
